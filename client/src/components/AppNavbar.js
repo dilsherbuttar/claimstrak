3b23079041c124095e55d7287e744df58d247ff7
Disher Buttar <buttar.dilsher@gmail.com>
2019-08-05 14:11:42 -0700

    Creates Navbar component

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,28 @@
+import React, { Component, Fragment } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from 'reactstrap';
+
+class AppNavBar extends Component {
+    state = {
+        isOpen: false
+    }
+
+    render() {
+        return (
+
+        <div>
+
+        </div>
+        )
+    }
+}
+
+export default AppNavBar;
